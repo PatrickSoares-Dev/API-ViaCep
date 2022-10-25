2e3f732c7cff1413239a91c733c1e9c625440178
Patrick Oliveira <patrick.oliveira@telecall.com>
2022-10-25 10:53:45 -0300

    API VIA CEP

---
+++ ICepApiService.cs
@@ -0,0 +1,18 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_Via_Cep
+{
+    public interface ICepApiService
+    {
+
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAdressAsync(string cep);
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace API_Via_Cep
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("http://viacep.com.br");
+                Console.WriteLine("Infome seu cep");
+
+                string cepInformado = Console.ReadLine().ToString();
+                Console.WriteLine("Consultando informações do CEP: " + cepInformado);
+
+                var adress = await cepClient.GetAdressAsync(cepInformado);
+
+                Console.Write($"\nLogradouro: {adress.Logradouro}" +
+                    $" \nBairro: {adress.Bairro}" +
+                    $"\nCidade: {adress.Localidade}" +
+                    $"\nLogradouro: {adress.Logradouro}" +
+                    $"\nComplemento: {adress.Complemento}");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error na consulta de cep: " + e.Message);
+            }
+
+
+        }
+    }
+}
